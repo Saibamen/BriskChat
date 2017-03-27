d771ad8a89a327a0e9939687d8a389540832b31c
Saibamen <saibamenppl@gmail.com>
2017-03-27 12:14:30 +0200

    Added Room, Tag and UserRoomTag models to BusinessLogic

---
+++ TrollChat.BusinessLogic/Models/Room.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace TrollChat.BusinessLogic.Models
+{
+    class Room : BaseModel
+    {
+        public User Owner { get; set; }
+
+        public List<Tag> Tags { get; set; }
+
+        public List<User> Users { get; set; }
+
+        public List<Message> Messages { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Topic { get; set; }
+
+        public int Customization { get; set; }
+
+        public bool IsPublic { get; set; }
+
+        public bool IsPrivateConversation { get; set; }
+    }
+}
