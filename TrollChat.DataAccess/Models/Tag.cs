42de86955c6f6cecfa16c2a927fc4b25ec010cdc
KrystianPodgorski <k.podgorski@praktyki.itmdevlabs.com>
2017-03-27 11:27:34 +0200

    Collection Tag

--- TrollChat.DataAccess/Models/Tag.cs
+++ TrollChat.DataAccess/Models/Tag.cs
@@ -1,10 +1,20 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TrollChat.DataAccess.Models
 {
     public class Tag : BaseEntity
     {
+        public Tag()
+        {
+            UserRoom = new HashSet<UserRoomTag>();
+            Room = new HashSet<RoomTag>();
+        }
+
+        public virtual ICollection<UserRoomTag> UserRoom { get; set; }
+        public virtual ICollection<RoomTag> Room { get; set; }
+        
         [Required]
         [Column(TypeName = "NVARCHAR(100)")]
         public string Name { get; set; }
