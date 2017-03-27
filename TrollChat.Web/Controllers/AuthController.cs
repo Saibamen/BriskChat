f4cded182c90ec78013b7b6b596e0fe96c442ba9
KrystianPodgorski <k.podgorski@praktyki.itmdevlabs.com>
2017-03-27 14:38:50 +0200

    Auth Controler, model and view

---
+++ TrollChat.Web/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrollChat.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ TrollChat.Web/Models/Auth/Register.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TrollChat.Web.Models.Auth
+{
+    public class Register
+    {
+        public class RegisterViewModel
+        {
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            public string Password { get; set; }
+        }
+    }
+}
