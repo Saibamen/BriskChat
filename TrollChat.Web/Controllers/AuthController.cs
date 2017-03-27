97975b44d1ad460c7459a40c1feebb7fee073904
Grzegorz <iamsopepsi@gmail.com>
2017-03-27 15:43:21 +0200

    TC-3 Added authorize action in BL

--- TrollChat.Web/Controllers/AuthController.cs
+++ TrollChat.Web/Controllers/AuthController.cs
@@ -1,26 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TrollChat.BusinessLogic.Actions.User.Interfaces;
-using TrollChat.BusinessLogic.Models;
 using TrollChat.Web.Models.Auth;
+using TrollChat.BusinessLogic.Models;
 
 namespace TrollChat.Web.Controllers
 {
     [Route("auth")]
     public class AuthController : BaseController
     {
+        private readonly IAuthorizeUser authorizeUser;
         private readonly IAddNewUser addNewUser;
 
-        public AuthController(IAddNewUser addNewUser)
+        public AuthController(IAuthorizeUser authorizeUser, IAddNewUser addNewUser)
         {
+            this.authorizeUser = authorizeUser;
             this.addNewUser = addNewUser;
         }
-
+        
         [HttpGet("register")]
         public IActionResult Register()
         {
             return View();
         }
 
+        [ValidateAntiForgeryToken]
         [HttpPost("register")]
         public IActionResult Register(RegisterViewModel model)
         {
@@ -35,6 +38,26 @@ namespace TrollChat.Web.Controllers
 
             // TODO: Alert: Confirmation email has been sent to your email address
             return RedirectToAction("Login", "Auth");
+        }
+
+        [HttpGet("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost("login")]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var access = authorizeUser.Invoke(model.Email, model.Password);
+
+            return View();
         }
     }
 }
