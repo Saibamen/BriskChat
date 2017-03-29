f5c344c833b74e8e01e37e9c63bc4d097e75d983
KrystianPodgorski <k.podgorski@praktyki.itmdevlabs.com>
2017-03-29 10:02:00 +0200

    Create Domain Model

---
+++ TrollChat.DataAccess/Models/Domain.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TrollChat.DataAccess.Models;
+
+namespace TrollChat.DataAccess.Models
+{
+    internal class Domain : BaseEntity
+    {
+    }
+}
