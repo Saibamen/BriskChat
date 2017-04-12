ffb3b5e4f415cce35a5bf6ea4b2220728f42677c
Saibamen <saibamenppl@gmail.com>
2017-04-12 13:14:30 +0200

    TC-32: Primitive ChannelHub

---
+++ TrollChat.Web/Hubs/ChannelHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace TrollChat.Web.Hubs
+{
+    public class ChannelHub : Hub
+    {
+        public void Send(string userName, string message)
+        {
+            Clients.All.broadcastMessage(userName, message);
+        }
+    }
+}
