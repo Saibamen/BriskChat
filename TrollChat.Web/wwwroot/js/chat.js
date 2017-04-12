b6b5a9e1e3836f2a0cb99774d72aa52855b44f31
Grzegorz <iamsopepsi@gmail.com>
2017-04-12 15:20:44 +0200

    Working SignalR

--- TrollChat.Web/wwwroot/js/chat.js
+++ TrollChat.Web/wwwroot/js/chat.js
@@ -10,60 +10,23 @@ $(".item > .btn_unstyle.right").popup({
     variation: "inverted"
 });
 
-/*
- *  SignalR
- */
+$.connection.hub.url = 'http://localhost:52284/signalr';
+var myHub = $.connection.channelHub;
 
-$(function () {
-    var connection = $.hubConnection("http://localhost:52283/signalr");
+myHub.client.broadcastMessage = function (username, message) {
+    console.log(message);
+}
+newConnection();
 
-    var testName = "TestName";
-
-    /*connection.broadcastMessage = function (userName, message) {
-        console.log("broadcastMessage  function");
-        var encodedName = $.text(userName).html();
-        var encodedMessage = $.text(message).html();
-
-        // TODO: add message to chat
-    };*/
-
-    // Start the connection
-    connection.start()
-        .done(function () {
-            console.log("Connected :)");
-            console.log("connection ID: " + connection.id);
-
-            $("#msg_form").keypress(function (e) {
-                if (e.which == 13) {
-                    console.log("Sending message " + $("#msg_input").val());
-                    connection.send(testName, $("#msg_input").val());
-                    $("#msg_input").val("");
-                }
-            });
-        })
-
-        .fail(function (a) {
-            console.log("Not connected! " + a);
-        });
-
-    connection.receive(function(data) {
-        console.log(data);
+function newConnection() {
+    $.connection.hub.start().done(function () {
+        
     });
+}
 
-    connection.error(function(error) {
-        console.warn(error);
-    });
-
-    connection.stateChanged(function (change) {
-        if (change.newState === $.signalR.connectionState.reconnecting) {
-            console.log("Re-connecting");
-        }
-        else if (change.newState === $.signalR.connectionState.connected) {
-            console.log("The server is online");
-        } 
-    });
-
-    connection.reconnected(function() {
-        console.log("Reconnected");
-    });
+$("#msg_form").keypress(function(e) {
+    if (e.which == 13) {
+        varmessage = $("#msg_input").val(); 
+        myHub.server.send(varmessage, varmessage);
+    }
 });
