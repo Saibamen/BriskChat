b34ffdec293e1d8ee388c56024e6ffdc4547c299
Saibamen <saibamenppl@gmail.com>
2017-04-12 15:41:53 +0200

    TC-32: More SignalR funtions for debugging

--- TrollChat.Web/wwwroot/js/chat.js
+++ TrollChat.Web/wwwroot/js/chat.js
@@ -10,23 +10,57 @@ $(".item > .btn_unstyle.right").popup({
     variation: "inverted"
 });
 
-$.connection.hub.url = 'http://localhost:52284/signalr';
-var myHub = $.connection.channelHub;
+/*
+ *  SignalR
+ */
 
-myHub.client.broadcastMessage = function (username, message) {
-    console.log(message);
-}
-newConnection();
+$(function () {
+    $.connection.hub.url = "http://localhost:52283/signalr";
+    var myHub = $.connection.channelHub;
 
-function newConnection() {
-    $.connection.hub.start().done(function () {
-        
-    });
-}
+    var testName = "TestName";
 
-$("#msg_form").keypress(function(e) {
-    if (e.which == 13) {
-        varmessage = $("#msg_input").val(); 
-        myHub.server.send(varmessage, varmessage);
+    myHub.client.broadcastMessage = function (username, message) {
+        console.log(testName + ": " + message);
     }
+
+    // Start the connection
+    $.connection.hub.start()
+        .done(function () {
+            console.log("Connected :)");
+            console.log("connection ID: " + $.connection.hub.id);
+
+            $("#msg_form").keypress(function (e) {
+                if (e.which == 13) {
+                    message = $("#msg_input").val(); 
+                    myHub.server.send(testName, message);
+                    $("#msg_input").val("");
+                }
+            });
+        })
+
+        .fail(function (a) {
+            console.log("Not connected! " + a);
+        });
+
+    $.connection.hub.received = function(data) {
+        console.log(data);
+    };
+
+    $.connection.hub.error = function(error) {
+        console.warn(error);
+    };
+
+    $.connection.hub.stateChanged(function (change) {
+        if (change.newState === $.signalR.connectionState.reconnecting) {
+            console.log("Re-connecting");
+        }
+        else if (change.newState === $.signalR.connectionState.connected) {
+            console.log("The server is online");
+        } 
+    });
+
+    $.connection.hub.reconnected(function() {
+        console.log("Reconnected");
+    });
 });
